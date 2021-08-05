0b23d7db7d1a1f65479dcfbe38b83385981e92e1
Avinash Rai <33398974+Avnshrai@users.noreply.github.com>
2021-08-05 12:45:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello GIIT Solution!!!'
+        return f'Hello there welcome to the GIIT solution!!!'
